66bc820933f5270e333076e03795e53a93a31c16
David <david@linuxsoftware.co.nz>
2020-08-20 04:59:35 +1200

    Joyous working with modeltranslations

--- home/translation.py
+++ home/translation.py
@@ -1,7 +1,12 @@
 from wagtail_modeltranslation.translation import TranslationOptions
 from modeltranslation.decorators import register
 from .models import HomePage
-from ls.joyous.models import GroupPage, CalendarPage, SimpleEventPage, MultidayEventPage, RecurringEventPage
+from ls.joyous.models import (
+        EventCategory, EventBase, CancellationBase,
+        GroupPage, CalendarPage, SimpleEventPage, MultidayEventPage,
+        RecurringEventPage, MultidayRecurringEventPage, ExtraInfoPage,
+        CancellationPage, PostponementPage, RescheduleMultidayEventPage,
+        ExtCancellationPage, ClosedForHolidaysPage)
 
 @register(HomePage)
 class HomeTR(TranslationOptions):
@@ -14,24 +19,81 @@ class HomeTR(TranslationOptions):
 @register(CalendarPage)
 class CalendarTR(TranslationOptions):
     fields = (
+        'intro',
+    )
+
+@register(EventCategory)
+class EventCategoryTR(TranslationOptions):
+    fields = (
+        'name',
     )
 
 @register(GroupPage)
 class GroupTR(TranslationOptions):
     fields = (
+        'content',
     )
 
 @register(SimpleEventPage)
 class SimpleEventTR(TranslationOptions):
     fields = (
+        'details',
     )
 
 @register(MultidayEventPage)
 class MultidayTR(TranslationOptions):
     fields = (
+        'details',
     )
 
 @register(RecurringEventPage)
 class RecurringEventTR(TranslationOptions):
+    fields = (
+        'details',
+    )
+
+@register(MultidayRecurringEventPage)
+class MultidayRecurringEventTR(TranslationOptions):
+    fields = (
+    )
+
+@register(ExtraInfoPage)
+class ExtraInfoTR(TranslationOptions):
+    fields = (
+        'extra_title',
+        'extra_information',
+    )
+
+@register(CancellationBase)
+class CancellationBaseTR(TranslationOptions):
+    fields = (
+        'cancellation_title',
+        'cancellation_details',
+    )
+
+@register(CancellationPage)
+class CancellationTR(TranslationOptions):
+    fields = (
+    )
+
+@register(PostponementPage)
+class PostponementTR(TranslationOptions):
+    fields = (
+        'postponement_title',
+        'details',
+    )
+
+@register(RescheduleMultidayEventPage)
+class RescheduleMultidayEventTR(TranslationOptions):
+    fields = (
+    )
+
+@register(ExtCancellationPage)
+class ExtCancellationTR(TranslationOptions):
+    fields = (
+    )
+
+@register(ClosedForHolidaysPage)
+class ClosedForHolidaysTR(TranslationOptions):
     fields = (
     )
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -131,17 +131,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
-LANGUAGE_CODE = 'fr'
-
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
+JOYOUS_DEFEND_FORMS=True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
